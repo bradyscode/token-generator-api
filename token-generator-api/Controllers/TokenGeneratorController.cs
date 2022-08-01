ca655f7254cd13dfe6bac5fcace41cd66782017e
BradysCode <bradystohler15@gmail.com>
2022-08-01 19:30:50 -0400

    Add project files.

---
+++ token-generator-api/Controllers/TokenGeneratorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace token_generator_api.Controllers
+{
+    public class TokenGeneratorController : Controller
+    {
+        [HttpGet("/token")]
+        public string Index()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
+
+//https://dev.to/jtenos/net-guid-formats-3koa
+//GUID Formats
